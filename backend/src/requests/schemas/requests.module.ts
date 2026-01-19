a3c80a3bc82ef376a5bca8ebf54f0d140d2b5efd
Pepito <pepito@pepito.com>
2026-01-19 16:13:30 -0300

    creacion archivo requests.module.ts para la gestion de features mongoose

---
+++ backend/src/requests/schemas/requests.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RequestsController } from '../requests.controller';   
+import { RequestsService } from '../requests.service';
+import {
+    SolicitudProteccion,
+    SolicitudProteccionSchema
+} from '../schemas/solicitud-proteccion.schema';
+import {
+    Negociacion,
+    NegociacionSchema
+} from '../schemas/negociacion.schema';
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            { name: SolicitudProteccion.name, schema: SolicitudProteccionSchema },
+            { name: Negociacion.name, schema: NegociacionSchema }
+        ])
+    ],
+    controllers: [RequestsController],
+    providers: [RequestsService],
+})
+export class RequestsModule {}
